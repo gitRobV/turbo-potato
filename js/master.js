593245daa2dc0997860a5d012826f07b851573e6
Robert Villarreal <robertv1979@gmail.com>
2017-06-02 18:09:26 -0700

    Completed Contact Card

--- js/master.js
+++ js/master.js
@@ -1,7 +1,28 @@
+
+
+function resetForm() {
+    $('input, textarea').val('');
+    $('.card').removeClass('new');
+}
+
+
+
 $(document).ready(function() {
 
-    $('.ninja img').click(function() {
-        $(this).fadeOut();
+    $('#contact_form').submit(function() {
+        var f_name = $('input[name=f_name]').val();
+        var l_name = $('input[name=l_name]').val();
+        var desc = $('textarea[name=desc]').val();
+        // Create card
+        $('#card_box').append('<div class="card new"></div>');
+        $('.new').append('<h2><a href="#">' + f_name + ' ' + l_name + '</a></h2>');
+        $('.new').append('<p class="hidden">' + desc + '</p>');
+        resetForm();
+        return false
+    })
+
+    $(document).on('click', '.card h2', function() {
+        $(this).siblings('p').toggle('slow');
     })
 
 });
